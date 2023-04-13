ea493d28c09054e0cd188bc6a3467f60c95eb053
pikachu-UwU <yandry2@uic.edu>
2023-04-12 20:23:05 -0500

    coudfare setup issues

--- hono/src/index.ts
+++ hono/src/index.ts
@@ -13,6 +13,10 @@ import { prettyJSON } from 'hono/pretty-json'
 import { z } from 'zod'
 import { zValidator } from '@hono/zod-validator'
 import * as jose from 'jose'
+import faunadb from 'faunadb';
+
+const faunaClient = new faunadb.Client({ secret: "fnAFBYEXE-AAUG-ngNcv0DP_Qs36eKVqCi3zBrLc" });
+const {Call, Function } = faunadb.query;
 
 const app = new Hono<{ Bindings: Bindings }>()
 
@@ -20,5 +24,37 @@ app.use('*', logger())
 app.use('*', prettyJSON())
 
 app.get('/', (c) => c.text('Shafa API v1.0.0'))
+
+/**
+ *  GET handling
+ * 
+ *  handle GET requests from user fetching all items
+ *  of specific type
+ */
+
+app.get('/items/:type', async (c) => {
+    try {
+        const type = await c.req.param("type");
+        const body = await c.req.json();
+
+        console.log('body', body);
+
+        // try to query the database:
+        const result = await faunaClient.query(
+            Call(
+                Function("getActiveItemsByType"),
+                body.user,
+                type
+            )
+        );
+        console.log(result);
+
+        return c.json(result);
+
+    } catch(error) {
+        console.log('error', error);
+        return c.json(error);
+    }
+})
 
 export default app

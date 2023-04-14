9661f2f0c4ebe34a0865b85affbc110bd787d321
Rechaud <ryang247@wisc.edu>
2023-04-14 01:23:02 -0500

    update commands

--- hono/src/index.ts
+++ hono/src/index.ts
@@ -29,46 +29,98 @@ app.use("*", prettyJSON());
 
 app.get("/", (c) => c.text("Shafa API v1.0.0"));
 
-// Retrieve all items in items collection
-app.get("/items", async (c) => {
-  try {
-    const { data } = await faunaClient.query(
-      Map(Paginate(Match(Index("allItems"))), Lambda("X", Get(Var("X"))))
-    );
-    return c.json(data);
-  } catch (error) {
-    console.error(error);
-    return c.text("Error occurred");
-  }
-});
+/** Leeza
+ *  GET handling
+ *
+ *  handle GET requests from user fetching all items
+ *  of specific type
+ */
 
-// Add a new item to the items collection
-app.post("/createItem", async (c) => {
-  try {
+app.get("/items/:type", async (c) => {
+    try {
+      const type = await c.req.param("type");
+      const { user } = await c.req.query();
+  
+      // try to query the database:
+      const result = await faunaClient.query(
+        Call(Function("getActiveItemsByType"), user, type)
+      );
+      // send response:
+      return c.json(result);
+    } catch (error) {
+      // something went wrong
+      return c.json(error);
+    }
+  });
+
+/** Richard
+ *  Update item
+ *
+ *  Calls updateItem function to Update an Item
+ */
+app.post("/updateItem", async (c) => {
+    try {
     // Convert the request body to a JSON object
-    const body = await c.req.json();
-    console.log(body);
-    const result = await faunaClient.query(
-      Call(
-        Function("addNewItem"),
-        body.user_id,
-        body.uuid,
-        body.desc,
-        body.brand,
-        body.photo,
-        body.primaryColor,
-        body.pattern,
-        body.type,
-        body.subtype,
-        body.style,
-        body.rating,
-        body.quality
-      )
-    );
-    return c.json(result);
-  } catch (error) {
-    return c.json(error);
-  }
-});
+      const body = await c.req.json();
+      console.log(body);
+      const result = await faunaClient.query(
+        Call(
+          Function("updateItem"),
+          body.user,
+          body.uuid,
+          body.desc,
+          body.brand,
+          body.photo,
+          body.primCol,
+          body.pattern,
+          body.type,
+          body.subtype,
+          body.style,
+          body.rating,
+          body.quality
+        )
+      );
+      return c.json(result);
+    } catch (error) {
+      return c.json(error);
+    }
+  });
+
+/** Richard
+ *  Update Outfit
+ *
+ *  Calls updateOutfit function to Update an Outfit
+ */
+  app.post("/updateOutfit", async (c) => {
+    try {
+    // Convert the request body to a JSON object
+      const body = await c.req.json();
+      console.log(body);
+      const result = await faunaClient.query(
+        Call(
+          Function("updateItem"),
+          body.uuid,
+          body.deptno,
+          body.dname,
+          body.loc,
+          body.username,
+          body.layer,
+          body.top,
+          body.bottom,
+          body.footwear,
+          body.accessories,
+          body.rating,
+        )
+      );
+      return c.json(result);
+    } catch (error) {
+      return c.json(error);
+    }
+  });
+
+
+
+
+
 
 export default app;
